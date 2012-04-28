87d8b4b1dd9394e96bb07fc469dea3c3f2436100
Guojun Xu <qinguan0619@gmail.com>
2012-04-29 00:48:14 +0800

    add a function for reversing the word order in one sentence.

---
+++ revSentence/revSentence.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+def revSentence(sentence):
+    """
+    the function is used to reverse the words order in one sentence.
+    """
+    words = sentence.split(' ')
+    words.reverse()
+    newsen = " ".join([item for item in words])
+    return newsen
+
+import unittest
+
+class revSentenceTestCase(unittest.TestCase):
+    """
+    test the function revSentence.
+    """
+     
+    def setUP(self):
+        pass
+    def tearDown(self):
+        pass
+
+    def testRevSentence(self):
+        self.assertEqual(revSentence("this is a real world"),"world real a is this")
+        self.assertEqual(revSentence("coding is full of fun"),"fun of full is coding")
+        
+if __name__ == '__main__':
+    unittest.main()
