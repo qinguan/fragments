c41c11b03e8f893c99def7e2a2bd0459efcc7d59
Guojun Xu <qinguan0619@gmail.com>
2012-05-01 16:13:40 +0800

    rm some Redundant code

--- pythonchangell/pythonchangell10.py
+++
@@ -1,9 +0,0 @@
-#import re
-#s="1"
-#for k in xrange(30):
-    #s="".join([str(len(i+j))+i for i,j in re.findall(r"(\d)(\1*)",s)])
-#print len(s)
-#look gif pics with Dynamic effects
-import os
-os.system(r"start C:\Users\qinguan\Desktop\rain.gif")
-
--- pythonchangell/pythonchangell11.py
+++
@@ -1,6 +0,0 @@
-import ImageEnhance
-import Image
-im=Image.open(r"C:\Users\qinguan\Desktop\cave.jpg")
-enhancer = ImageEnhance.Brightness(im)
-factor = 8
-enhancer.enhance(factor).show()
