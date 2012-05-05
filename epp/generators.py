ebfc0640f8517833d1f5d3f565da030a23aaf112
Guojun Xu <qinguan0619@gmail.com>
2012-05-05 21:41:27 +0800

    add a example of coroutine

--- epp/generators.py
+++ epp/generators.py
@@ -7,3 +7,24 @@ def fibonacci():
         a, b = b, a+b
 fib = fibonacci()
 print [fib.next() for i in range(10)]
+
+#********************************************#
+import multitask
+import time
+
+def coroutine_1():
+    for i in range(3):
+        print 'c1'
+        yield
+
+def coroutine_2():
+    for i in range(3):
+        print 'c2'
+        yield i
+
+multitask.add(coroutine_1())
+multitask.add(coroutine_2())
+multitask.run()
+
+#********************************************#
+#from __future__ import with_statement
