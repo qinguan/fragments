7dea51b8b5afc1acd0735479d4cba5d8b6f32001
Guojun Xu <qinguan0619@gmail.com>
2012-05-20 12:46:04 +0800

    add a function generating the absolute path of files with given directory

--- tools/walk_directory.py
+++ tools/walk_directory.py
@@ -1,13 +1,22 @@
 #! /usr/bin/env python
 
-def _walk_directory(root_directory):
+def walk_directory(root_directory):
     """
-    Generates the paths of all that are ancestors of `root_directory`.
+    Generates the absolute path of all files in root_directory`.
     """
     import os
     paths = [os.path.join(root, name)
              for root, dirs, files in os.walk(root_directory)
              for name in files]
-
     paths.sort()
+    if paths is None:
+        print 'hello'
     return paths
+
+def test_walk_directory():
+    import os
+    print  walk_directory(os.getcwd()) 
+#    print  walk_directory(os.path.dirname(os.getcwd()))
+
+if __name__ == '__main__':
+    test_walk_directory()
